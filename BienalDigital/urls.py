8e7e8ff1f106b1e49c87943122966f83a1608c3b
Costantini Nestor Matias <costantini.nestor.m@gmail.com>
2020-08-26 21:35:32 -0300

    Se creo el archivo base.html y se implementaron los tag head, header y footer en el index.html.

--- BienalDigital/urls.py
+++ BienalDigital/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view.Home, name='inicio'),
+
 ]
