d88b25e57bba1aaffb6bac19bc78580044ab63f9
Costantini Nestor Matias <costantini.nestor.m@gmail.com>
2020-09-03 12:42:58 -0300

    Se craeron lo archivo html del registro, login y seccion de usuarios, se aplico la funcionalidad de loguearse y regitrarse.

--- BienalDigital/view.py
+++ BienalDigital/view.py
@@ -1,4 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as do_logout
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from apps.usuarios.models import CustomUserForm
+from django.http import HttpResponseRedirect
+# Create your views here.
 def Home(request):
     return render(request, 'index.html')
+    
+@login_required
+def welcome(request):
+     # Si estamos identificados devolvemos la portada
+    if request.user.is_authenticated:
+        return render(request, "registration/welcome.html")
+    # En otro caso redireccionamos 
+    return render(request, "registration/welcome.html")
+
+def register(request):
+    data={
+        'form':CustomUserForm()
+    }
+    if request.method=='POST':
+        formulario=CustomUserForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            #autenticar al usuario
+            username = formulario.cleaned_data['username']
+            password = formulario.cleaned_data['password1']
+            user= authenticate(username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect('')
+        else:
+            print("Un error ocurrio, vuelva a intentar")
+    return render(request, "registration/register.html", data)
+
---
+++ apps/usuarios/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class Paises(models.Model):
+    id_pais=models.IntegerField()
+    pais=models.TextField(max_length=20)
+    def getId(self):
+        return self.id_pais
+    def getPais(self):
+        return self.pais
+
+class Sexo(models.Model):
+    id_sexo=models.IntegerField()
+    sexo=models.TextField()
+    def getSexo(self):
+        return self.sexo
+    def getId(self):
+        return self.id_sexo
+
+class CustomUserForm(UserCreationForm): 
+    pass
+
+class Perfil():
+    id=models.IntegerField()
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    fecha_nacimiento=models.DateField()
+    sexo=models.ForeignKey(Sexo, on_delete=models.DO_NOTHING)
+    dni=models.IntegerField()
+    nacionalidad= models.ForeignKey(Paises, on_delete=models.DO_NOTHING)
+    """foto=models.URLField()"""
+
+    def getUser(self):
+        return self.user
+
+    def getFecha(self):
+        return self.fecha_nacimiento
+    
+    def getSexo(self):
+        return self.sexo
+    
+    def getDni(self):
+        return self.dni
+
+    def getNacionalidad(self):
+        return self.nacionalidad
