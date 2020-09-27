ad08ff8e9327b085d85a326b265200ea51cbbc2e
Costantini Nestor Matias <costantini.nestor.m@gmail.com>
2020-09-27 18:27:03 -0300

    Ahora se puede seleccionar la categoria en la que se de sea visualizar los votos y se los muestra ordenado de mayor a menor.

--- apps/votos/urls.py
+++ apps/votos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import mostrarVotos, votar
+from .views import mostrarVotos, votar, votar_ajax
 
 urlpatterns = [
     path('mostrar-votos/<int:id>', mostrarVotos, name='mostrarVotos'),
     path('votar/', votar, name='votar'),
-    
+    path('votar_ajax/', votar_ajax, name='votar_ajax')
+
 ]
