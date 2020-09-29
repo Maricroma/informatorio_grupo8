7d5e4783b740c43e6db2046c3f69404314f2121d
Maricroma <cris.apdg@gmail.com>
2020-09-29 17:37:51 -0300

    Se agregó el alerta y validación con el usuario al momento de emitir voto

--- static/js/confirmacion.js
+++ static/js/confirmacion.js
@@ -8,11 +8,14 @@ function confirmarVoto(user_id, video_id) {
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, estoy seguro',
+        confirmButtonText: 'Sí, votar',
         cancelButtonText: 'Cancelar'
       }).then((result) => {
         if (result.isConfirmed) {
           Swal.fire(
+            'Hecho!',
+            'Tu voto ha sido registrado.',
+            'success',
             votar(user_id, video_id)
           )
         }
