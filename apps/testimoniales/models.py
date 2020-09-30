e9461117e8bfb2885a3288d9ff843e2743bda5a7
Dihue <dihue21@gmail.com>
2020-09-30 00:17:24 -0300

    En funcionamiento la app testimoniales

--- apps/testimoniales/models.py
+++ apps/testimoniales/models.py
@@ -1,3 +1,10 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Comentario(models.Model):
+	nombre = models.CharField(max_length=80)
+	email = models.EmailField()
+	mensaje = models.TextField(max_length=500)
+	
+	def __str__(self):
+		return self.nombre
